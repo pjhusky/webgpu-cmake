9f197c1ee78cbd3fc3d61fd7a3858300a14fbd00
P H <p.houska@cgv.tugraz.at>
2024-11-07 17:02:31 +0100

    Basic building with Dawn WebGPU works, but dawn dll is not copied to exe dir yet

---
+++ build_win.bat
@@ -0,0 +1,2 @@
+cmake -S . -B .\build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=C:\DEV\WebGPU_dawn\dawn\install\Release
+cmake --build .\build --config Release
---
+++ hello_webgpu.cpp
@@ -0,0 +1,53 @@
+#include <webgpu/webgpu_cpp.h>
+#include <dawn/webgpu_cpp_print.h>
+
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  wgpu::InstanceDescriptor instanceDescriptor{};
+  instanceDescriptor.features.timedWaitAnyEnable = true;
+  wgpu::Instance instance = wgpu::CreateInstance(&instanceDescriptor);
+  if (instance == nullptr) {
+    std::cerr << "Instance creation failed!\n";
+    return EXIT_FAILURE;
+  }
+  // Synchronously request the adapter.
+  wgpu::RequestAdapterOptions options = {};
+  wgpu::Adapter adapter;
+  wgpu::RequestAdapterCallbackInfo callbackInfo = {};
+  callbackInfo.nextInChain = nullptr;
+  callbackInfo.mode = wgpu::CallbackMode::WaitAnyOnly;
+  auto myCallback = [](WGPURequestAdapterStatus status,
+                             WGPUAdapter adapter, 
+                              //const char *message,
+                              WGPUStringView message,
+                             void *userdata) {
+    if (status != WGPURequestAdapterStatus_Success) {
+      std::cerr << "Failed to get an adapter:" << message.data;
+      return;
+    }
+    *static_cast<wgpu::Adapter *>(userdata) = wgpu::Adapter::Acquire(adapter);
+  };
+  callbackInfo.callback = /*(wgpu::RequestAdapterCallback)*/myCallback;
+  callbackInfo.userdata = &adapter;
+  instance.WaitAny(instance.RequestAdapter(&options, callbackInfo), UINT64_MAX);
+  if (adapter == nullptr) {
+    std::cerr << "RequestAdapter failed!\n";
+    return EXIT_FAILURE;
+  }
+
+  wgpu::DawnAdapterPropertiesPowerPreference power_props{};
+
+  wgpu::AdapterInfo info{};
+  info.nextInChain = &power_props;
+
+  adapter.GetInfo(&info);
+  std::cout << "VendorID: " << std::hex << info.vendorID << std::dec << "\n";
+  std::cout << "Vendor: " << info.vendor << "\n";
+  std::cout << "Architecture: " << info.architecture << "\n";
+  std::cout << "DeviceID: " << std::hex << info.deviceID << std::dec << "\n";
+  std::cout << "Name: " << info.device << "\n";
+  std::cout << "Driver description: " << info.description << "\n";
+  return EXIT_SUCCESS;
+}
