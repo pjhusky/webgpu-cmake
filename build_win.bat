59a5ec47be323b63a4012dfb93f1868fb874c76b
P H <p.houska@cgv.tugraz.at>
2024-11-07 19:32:59 +0100

    added a small reminder to the build script to set the path to the Dawn install directory

--- build_win.bat
+++ build_win.bat
@@ -1,2 +1,3 @@
+@ECHO: Make sure to define the path to the Dawn install dir
 cmake -S . -B .\build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=C:\DEV\WebGPU_dawn\dawn\install\Release
 cmake --build .\build --config Release
