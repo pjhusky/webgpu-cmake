5cc53ba9911d6fd2b12934fdb4e23dc498a3baaf
P H <p.houska@cgv.tugraz.at>
2024-11-08 04:16:49 +0100

    no longer including all CMAKE_PREFIX_PATH ´paths when building WGPU_Native backend * minor cleanup of CMakeLists.txt
    no longer including all CMAKE_PREFIX_PATH ´paths when building WGPU_Native backend
    * minor cleanup of CMakeLists.txt

--- build_win.bat
+++ build_win.bat
@@ -1,2 +1,8 @@
 @ECHO: Make sure to define the path to the Dawn install dir
-cmake -S . -B .\build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\DEV\WebGPU_dawn\dawn\install\Release;C:\DEV\WebGPU_wgpu-native-C-from-rust\wgpu-native\meson-install-release" -DWEBGPU_IMPL_STR="WEBGPU_WGPU_NATIVE" && cmake --build .\build --config Release
+setlocal
+
+@REM 
+set WEBGPU_IMPL_STR="WEBGPU_WGPU_NATIVE"
+@REM set WEBGPU_IMPL_STR="WEBGPU_DAWN"
+
+cmake -S . -B .\build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\DEV\WebGPU_dawn\dawn\install\Release;C:\DEV\WebGPU_wgpu-native-C-from-rust\wgpu-native\meson-install-release" -DWEBGPU_IMPL_STR=%WEBGPU_IMPL_STR% -DWEBGPU_WGPU_NATIVE_BASE_DIR="C:\DEV\WebGPU_wgpu-native-C-from-rust\wgpu-native\meson-install-release" && cmake --build .\build --config Release
