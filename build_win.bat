49168b82b58856441bcfb29c11bfd774aa4ccfbd
P H <p.houska@cgv.tugraz.at>
2024-11-09 12:25:45 +0100

    added a 'clean' build argument

--- build_win.bat
+++ build_win.bat
@@ -1,8 +1,21 @@
 @ECHO: Make sure to define the path to the Dawn install dir
 setlocal
 
+@REM set WEBGPU_IMPL_STR="WEBGPU_WGPU_NATIVE"
 @REM 
-set WEBGPU_IMPL_STR="WEBGPU_WGPU_NATIVE"
-@REM set WEBGPU_IMPL_STR="WEBGPU_DAWN"
+@set WEBGPU_IMPL_STR="WEBGPU_DAWN"
+@set MY_CMAKE_PREFIX_PATH="C:\DEV\WebGPU_dawn\dawn\install\Release;C:\DEV\WebGPU_wgpu-native-C-from-rust\wgpu-native\meson-install-release" 
+@set MY_WEBGPU_WGPU_NATIVE_BASE_DIR="C:\DEV\WebGPU_wgpu-native-C-from-rust\wgpu-native\meson-install-release"
+@set CMAKE_GEN_BUILD_FILE_CMD=cmake -S . -B .\build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%MY_CMAKE_PREFIX_PATH% -DWEBGPU_IMPL_STR=%WEBGPU_IMPL_STR% -DWEBGPU_WGPU_NATIVE_BASE_DIR=%MY_WEBGPU_WGPU_NATIVE_BASE_DIR%
 
-cmake -S . -B .\build -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:\DEV\WebGPU_dawn\dawn\install\Release;C:\DEV\WebGPU_wgpu-native-C-from-rust\wgpu-native\meson-install-release" -DWEBGPU_IMPL_STR=%WEBGPU_IMPL_STR% -DWEBGPU_WGPU_NATIVE_BASE_DIR="C:\DEV\WebGPU_wgpu-native-C-from-rust\wgpu-native\meson-install-release" && cmake --build .\build --config Release
+@set OPTIONAL_TARGET=
+
+IF "%~1"=="" (
+    @echo No argument passed.
+) ELSE IF "%~1"=="clean" (
+    @echo Argument is 'clean'.
+    @set OPTIONAL_TARGET="--target clean"
+) ELSE (
+    @echo Argument is not 'clean'.
+)
+%CMAKE_GEN_BUILD_FILE_CMD% && cmake --build .\build --config Release %OPTIONAL_TARGET% 
